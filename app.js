4aafe3af5121f12026513354ecaf501d601a7177
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-01-24 21:56:50 -0300

    Created 'express.Routes()'

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Todo ok');
-});
+const routeApi = require('./routes/api'); // Routes file
+
+app.use(routeApi); // Routes file
 
 //Listening server
 app.listen(PORT, () => console.log(`Listening server on port ${PORT}`))
