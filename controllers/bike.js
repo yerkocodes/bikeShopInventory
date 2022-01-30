aba2a1335f330bd0e94a9d1acc802f6722eb6394
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-01-30 18:49:21 -0300

    Added controllers API

--- controllers/bike.js
+++ controllers/bike.js
@@ -2,4 +2,53 @@ const pool = require('../db/init').getPoolInstance();
 
 module.exports = {
   //Database queries
+  getStores: ( req, res ) => {
+    pool.connect( async ( err_connect, client, release ) => {
+
+      const SQLQuery = `select * from stores`;
+      try {
+        const response = await client.query(SQLQuery);
+        const stores = response.rows;
+        //console.log(stores);
+        res.send(stores);
+      } catch ( err ) {
+        console.log(err.message);
+      } finally {
+        release();
+      };
+    } );
+
+  },
+
+  getCategories: ( req, res ) => {
+    pool.connect( async ( err_connect, client, release ) => {
+      const SQLQuery = `select * from categories`;
+
+      try {
+        const response = await client.query(SQLQuery);
+        const categories = response.rows;
+        res.send(categories);
+      } catch ( err ) {
+        console.log(err.message);
+      } finally {
+        release()
+      };
+    } );
+  },
+
+  getBrands: ( req, res ) => {
+    pool.connect( async ( err_connect, client, release ) => {
+      const SQLQuery = `select * from brands`;
+
+      try {
+        const response = await client.query(SQLQuery);
+        const brands = response.rows;
+        res.send(brands);
+      } catch ( err ) {
+        console.log(err.message);
+      } finally {
+        release();
+      };
+    } );
+  },
 };
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const controllersBikeShop = require('../controllers/bike');
 
 router.get('/', (req, res) => {
   res.send('Todo ok');
 });
+
+router.get('/api/stores', controllersBikeShop.getStores);
+
+router.get('/api/categories', controllersBikeShop.getCategories);
+
+router.get('/api/brands', controllersBikeShop.getBrands);
 
 module.exports = router;
